79f2970000c6fdfb1a335999b329cd8daf998c24
ProYield Community <proyield@pyd.fi>
2026-09-18 23:58:44 -0400

    Add Anvil network config for hyperTestnet

--- hardhat.config.js
+++ hardhat.config.js
@@ -3,6 +3,12 @@ require("@nomicfoundation/hardhat-toolbox");
 /** @type import('hardhat/config').HardhatUserConfig */
 module.exports = {
   solidity: "0.8.28",
+  networks: {
+    hyperTestnet: {
+      url: "http://localhost:8545",
+      chainId: 998,
+    },
+  },
   paths: {
     contracts: "./contracts",
     tests: "./test",
