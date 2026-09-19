e642b94b5b9105789564c7189ce23f6b6306d9b9
ProYield Community <proyield@pyd.fi>
2026-09-19 03:38:10 -0400

    Update keeper_action.js with latest contract addresses

--- scripts/keeper_action.js
+++ scripts/keeper_action.js
@@ -3,7 +3,7 @@ const hre = require("hardhat");
 async function main() {
   const [owner] = await hre.ethers.getSigners();
   const V = await hre.ethers.getContractFactory("ProYieldVault");
-  const v = V.attach("0x73278b87e51595CC5F41B3132B595a524526Db76");
+  const v = V.attach("0xe7f1725E7734CE288F8367e1Bb143E90bb3F0512");
   console.log("totalAssets", hre.ethers.formatUnits(await v.totalAssets(), 18), "USDC");
   try {
     const h = await v.harvest();
